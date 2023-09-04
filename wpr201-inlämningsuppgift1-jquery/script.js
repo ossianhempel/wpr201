6dd4458bb49a2365e03bba5fed54d79b53083e41
ossianhempel <hemposse@hotmail.com>
2023-09-04 21:52:41 +0200

    Add changeTextOnDoubleClick and toggleElementOnClick

--- wpr201-inlämningsuppgift1-jquery/script.js
+++ wpr201-inlämningsuppgift1-jquery/script.js
@@ -1,43 +1,40 @@
-console.log('Script file found');
-
-
 // Samla alla jQuery funktioner så de körs när sidan är laddad
 $(document).ready(function() {
-console.log('jQuery is activated');
-
-  toggleElementOnClick("#toggle-element");
+  // Kontrollerar att jQuery är korrekt kopplat
+  console.log('jQuery is activated');
+  
+  toggleElementOnClick("#self-description");
   changeStyleOnHover("#self-description");
   performInitialAnimation("#contentbox-middle");
-  changeTextOnDoubleClick("#trigger-element", "#text-element");
+  changeTextOnDoubleClick("#contentbox-middle", "#contentbox-middle .contentbox-text");
   toggleDarkMode();  
 });
 
-// 1. Toggle visibility of an element when it's clicked
+// 1. Toggla synligheten på ett element som klickas på
 function toggleElementOnClick(selector) {
   $(selector).click(function() {
     $(this).toggle();
   });
 }
 
-// 2. Change the style of an element when the mouse hovers over it
+// 2. Ändrar stilen på ett element när man hoverar på det
 function changeStyleOnHover(selector) {
   $(selector).hover(function() {
     $(this).toggleClass("new-style");
   });
 }
 
-// 3. Perform some animation on an element
+// 3. Animerar ett element att "floata" nedåt till höger och tillbaka vid page load
 function performInitialAnimation(selector) {
-  console.log('Animation triggered');
   
   $(selector).animate({left: '100px', top: '100px'}, 1000)
              .animate({left: '0px', top: '0px'}, 1000);
 }
 
-// 4. Change the text of an element when another element is double-clicked
+// 4. Ändrar texten på ett element när det eller ett annat specificerat element dubbelklickas på
 function changeTextOnDoubleClick(triggerSelector, targetSelector) {
   $(triggerSelector).dblclick(function() {
-    $(targetSelector).text("New Text");
+    $(targetSelector).text("Du dubbelklickade på mig...nu ser jag ut såhär istället!");
   });
 }
 
